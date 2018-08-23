d2d623b64040e43e33ed0acc1319cdd9fcb6759a
Mark Stringer <mark.stringer@mumbly.co.uk>
2018-08-23 12:10:47 +0100

    Add files required for weekly report.

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 git add *.md *.png *.svg *.dat *.gnu *.jpg *.sh *.txt *.pl
+git add graphs/*.png graphs/*.svg graphs/*.dat graphs/*.txt graphs/*.jpg graphs/*.gnu
 git commit -m "Add files required for weekly report. ${@}"
 git push
