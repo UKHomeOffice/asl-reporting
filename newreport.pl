70b4698fb81784c1d9c2ae82c9a37704132b290d
Mark Stringer <mark.stringer@mumbly.co.uk>
2020-06-04 11:47:46 +0100

    change the newreport.pl script so that it adds in the goals

--- newreport.pl
+++ newreport.pl
@@ -39,12 +39,12 @@ my $report_file = "report$date_short.md";
 
 copy($TEMPLATE,$report_file) or die "Copy failed: $!";
 
-my $file = path($report_file);
-my $data = $file->slurp_utf8;
-$data =~ s/DATE_SHORT/$date_short/g;
-$data =~ s/DATE_LONG/$date_long/g;
-$data =~ s/SPRINT_NO/$sprint/g;
-$file->spew_utf8( $data );
+##my $file = path($report_file);
+##my $data = $file->slurp_utf8;
+##$data =~ s/DATE_SHORT/$date_short/g;
+##$data =~ s/DATE_LONG/$date_long/g;
+##$data =~ s/SPRINT_NO/$sprint/g;
+##$file->spew_utf8( $data );
 
 my $bash_command = "bash getscreenshot.sh $username $password \'$SPRINT_STORIES\'";
 say $bash_command;
@@ -114,6 +114,41 @@ $driver->get($DONE);
 $driver->pause(5000);
 $element = $driver->find_element('//*[@class=\'results-count-total results-count-link\']');
 my $done = $element->get_text();
+
+
+## get the current spring goals
+$driver->pause(5000);
+$driver->get('https://collaboration.homeoffice.gov.uk/jira/secure/RapidBoard.jspa?rapidView=1706');
+$element=  $driver->find_element('//span[@id=\'ghx-sprint-goal\']');
+my $currentGoals = $element->get_text();
+$currentGoals =~ s/(\d)\)/\n$1./g;
+$currentGoals = "## These are the goals for the current sprint:\n".$currentGoals;
+say "Current goals are: ".$currentGoals;
+
+## get the previous sprint goals
+$driver->pause(5000);
+$driver->get('https://collaboration.homeoffice.gov.uk/jira/secure/RapidBoard.jspa?rapidView=1706&view=reporting&chart=sprintRetrospective');
+$driver->pause(5000);
+$element=  $driver->find_element('//div[@class=\'ghx-sprint-goal\']');
+my $previousGoals = $element->get_text();
+$previousGoals =~ s/(\d)\)/\n$1./g;
+$previousGoals = "## These were the goals for the previous sprint:\n".$previousGoals;
+say "Previous goals are: ".$previousGoals;
+
+
+copy($TEMPLATE,$report_file) or die "Copy failed: $!";
+my $file = path($report_file);
+my $data = $file->slurp_utf8;
+$data =~ s/DATE_SHORT/$date_short/g;
+$data =~ s/DATE_LONG/$date_long/g;
+$data =~ s/SPRINT_NO/$sprint/g;
+$data =~ s/CURRENT_SPRINT_GOALS/$currentGoals/g;
+$data =~ s/PREVIOUS_SPRINT_GOALS/$previousGoals/g;
+
+$file->spew_utf8( $data );
+
+## write them in the report 
+
 open(my $fh, '>>', $PROGRESS_DAT_FILE) or die "Could not open file '$PROGRESS_DAT_FILE' $!";
 say $fh "$date_for_seconds $done $doing $todo";
 close $fh;
