8d849b06eb1041fb39e3921df327940734ccc946
Mark Stringer <mark.stringer@mumbly.co.uk>
2020-10-14 11:53:34 +0100

    Add files required for weekly report.

---
+++ graphs/histograms.2.gnu
@@ -0,0 +1,10 @@
+set terminal png size 800,600 enhanced font "Serif,10"
+set output 'roadmap.png'
+set style fill solid
+set style data histogram
+set style histogram rowstack gap 1
+set style fill solid border -1
+set boxwidth 0.9
+set key autotitle columnheader
+set xtics border in scale 0,0 nomirror rotate by -45  autojustify
+plot 'roadmap.dat' using 2:xtic(1) ti col , '' using 3 ti col, '' using 4 ti col
--- roadmapJSON2.pl
+++ roadmapJSON2.pl
@@ -6,6 +6,7 @@ use Cwd;
 use lib cwd;
 use JSON::Parse 'json_file_to_perl';
 use Data::Dumper;
+use Tally;
 my $file = shift || die "No filename";
 
 my $p = json_file_to_perl ($file);
@@ -20,19 +21,103 @@ my $DONE = 'Done';
 my %cards_hash;
 my %lists_hash;
 my @cards = @{$p->{cards}};
-foreach my $card (@cards)
-{
-        $cards_hash{$card->{name}} = $card->{idList};
-}
+my $publishing = new Tally("Publishing", 0,0,0,0);
+my $enhancement = new Tally("Enhancements", 0,0,0,0);
+my $history = new Tally("History", 0,0,0,0);
+my $compliance=new Tally("Compliance", 0,0,0,0);
+my $report= new Tally("Reports", 0,0,0,0);
+my $referrals= new Tally("Referrals", 0,0,0,0);
+
 
 my @lists = @{$p->{lists}};
 foreach my $list (@lists)
 {
-	if (!$list->{closed})
-	{	
-		$lists_hash{$list->{id}} = $list->{name};
-	}
+        if (!$list->{closed})
+        {
+                $lists_hash{$list->{id}} = $list->{name};
+		say "List name is: $list->{name}";
+        }
 }
+
+foreach my $card (@cards)
+{
+	my $points = 0;
+	my $percentage = 0;
+	my $cardName = $card->{name};
+	my $idList = $card->{idList};
+        if (!exists $lists_hash{$idList})
+	{
+		goto NEXT;
+	}
+	say "List is: $lists_hash{$idList}";
+	if($cardName =~  /\[\s*?Not\s*?Done\s*?\]/i)
+	{
+		say "Ignoring $cardName";
+		goto NEXT;
+	}
+       
+	if($cardName =~ /\[\s*?(\d+)\s*?Points\s*?\]/i)
+        {
+        	$points = $1;
+                say "Points are $points";
+                if ($cardName =~ /\[\s*?(\d+)\s*?\%\s*?\]/)
+                {
+                	$percentage = $1;
+                        say "Percentage is $percentage";
+                }
+        }
+	elsif($lists_hash{$idList} =~ /Done/)
+	{
+		$points = 144;
+		$percentage = 100;
+	}
+
+
+	my @label_list = @{$card->{labels}};
+	my $category = $label_list[0]->{name} || goto NEXT;
+	## sort the cards into buckets
+	if($category =~ /publishing/i)
+	{
+		$publishing->doTally($points, $percentage);
+	}
+	elsif($category=~ /enhancement/i)
+	{
+		$enhancement->doTally($points, $percentage);
+	}
+	elsif($category=~ /history/i)
+	{
+		$history->doTally($points, $percentage);
+	}
+	elsif($category=~ /compliance/i)
+	{
+		$compliance->doTally($points, $percentage);
+	}
+	elsif($category=~ /report/i)
+	{
+		$report->doTally($points, $percentage);
+	}
+	elsif($category=~ /referrals/i)
+	{
+		$referrals->doTally($points, $percentage);
+	}
+	else
+	{
+		goto NEXT;
+	}
+	say($cardName);
+	
+	NEXT:
+}
+
+say($publishing->getTally());
+say($enhancement->getTally());
+say($history->getTally());
+say($compliance->getTally());
+say($report->getTally());
+say($referrals->getTally());
+
+
+exit(0);
 
 foreach my $cardName (keys %cards_hash)
 {
